0c15fbbc9cca1fe156f7e995b2044f1647f247a2
Ravi Gavarasana <gavarasana@gmail.com>
2018-11-07 19:55:20 -0500

    Creating exchange, queue and binding

---
+++ ravi.learn.rmq.console/ravi.learn.rmq.console/Program.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ravi.learn.rmq.console
+{
+    class Program
+    {
+        private const string RMQ_HOST = "localhost";
+        private const string RMQ_USERNAME = "guest";
+        private const string RMQ_PWD = "guest";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating RabbitMQ exchanges and queues");
+
+            try
+            {
+                var connectionFactory = new ConnectionFactory
+                {
+                    HostName = RMQ_HOST,
+                    UserName = RMQ_USERNAME,
+                    Password = RMQ_PWD
+                };
+
+                var connection = connectionFactory.CreateConnection();
+                var model = connection.CreateModel();
+
+                var properties = model.CreateBasicProperties();
+                properties.Persistent = false;
+
+                var serializedMessage = Encoding.Default.GetBytes("This is the first message");
+
+                model.BasicPublish("MyFirstExchange", "cars", properties, serializedMessage);
+
+                Console.WriteLine("Published basic message to MyFirstExchange");
+
+               // CreateQueueNExchange(model);
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void CreateQueueNExchange(IModel model)
+        {
+            model.ExchangeDeclare("MyFirstExchange", ExchangeType.Topic, true, false, null);
+
+            model.QueueDeclare("MyFirstQueue", true, false, false, null);
+
+            model.QueueBind("MyFirstQueue", "MyFirstExchange", "cars");
+
+            Console.WriteLine("Created exchange, queue and binding");
+        }
+    }
+}
